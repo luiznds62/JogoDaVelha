f2c479b5e13d29d003ca9913664937203674aae4
luiznds62 <42879594+luiznds62@users.noreply.github.com>
2019-11-23 16:41:34 -0200

    v1

---
+++ main.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow} = require('electron')
+    const url = require("url");
+    const path = require("path");
+
+    let mainWindow
+
+    function createWindow () {
+      mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "Jogo da Velha - Redes",
+        icon: __dirname + "./src/assets/oldwomanico.ico",
+        webPreferences: {
+          nodeIntegration: true
+        },
+        autoHideMenuBar: true
+      })
+
+      mainWindow.loadURL(
+        url.format({
+          pathname: path.join(__dirname, `/dist/index.html`),
+          protocol: "file:",
+          slashes: true
+        })
+      );
+      
+      // Open the DevTools.
+      //mainWindow.webContents.openDevTools()
+
+      mainWindow.on('closed', function () {
+        mainWindow = null
+      })
+    }
+
+    app.on('ready', createWindow)
+
+    app.on('window-all-closed', function () {
+      if (process.platform !== 'darwin') app.quit()
+    })
+
+    app.on('activate', function () {
+      if (mainWindow === null) createWindow()
+    })
